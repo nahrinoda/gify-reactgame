cce0cd63c11840d77030241a11fafc132499c831
nahrinoda <nahrinoda@yahoo.com>
2018-12-07 16:30:13 -0500

    Updates

---
+++ precache-manifest.b21c11ab0abe0b4bfedd552d4f6e0b78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "83125558c56b46cb4e80",
+    "url": "/gify-reactgame/static/css/main.1c768391.chunk.css"
+  },
+  {
+    "revision": "83125558c56b46cb4e80",
+    "url": "/gify-reactgame/static/js/main.83125558.chunk.js"
+  },
+  {
+    "revision": "4c6dc1f2fde3e77db660",
+    "url": "/gify-reactgame/static/js/1.4c6dc1f2.chunk.js"
+  },
+  {
+    "revision": "ae45ecc609b29d7da035",
+    "url": "/gify-reactgame/static/js/runtime~main.ae45ecc6.js"
+  },
+  {
+    "revision": "703bcfe14962cd745c0ad194bf2c75f5",
+    "url": "/gify-reactgame/index.html"
+  }
+];
